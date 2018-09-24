1b8388abefb1efebc002fbb24562f415f1d1c04f
courtney persons <personscourtney@gmail.com>
2018-09-24 20:13:34 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,5 @@
 # Build your say_hello method here
 
-def say_hello(name, langues = "Ruby Programmer!")
+def say_hello(name = "Ruby Programmer!")
 puts "Hello #{name}!"
-# puts "Hello #{langues}"
 end
